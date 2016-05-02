4a71061df068ebd576c9790304099ec181d2aa7e
Patrick thibaudeau <thibaud@glendon.yorku.ca>
2016-05-02 07:24:39 -0400

    Upgraded version to release candidate

--- version.php
+++ version.php
@@ -25,6 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016050101;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2016050200;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2015111000;        // Requires this Moodle version.
 $plugin->component = 'format_glendon';    // Full name of the plugin (used for diagnostics).
+$plugin->maturity = MATURITY_RC;
