f2bc0175e6f0349acc53b49de56e48590ec32aa1
Patrick thibaudeau <thibaud@glendon.yorku.ca>
2016-05-02 10:41:55 -0400

    Added French language file

---
+++ lang/fr/format_glendon.php
@@ -0,0 +1,51 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'format_glendon', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   format_glendon
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['bootstrap_version'] = 'Version Bootstrap';
+$string['bootstrap_version_help'] = 'Sélectionnez la version de bootstrap actuellement utilisé dans votre thème.';
+$string['collapsed'] = 'Première section fermé?' ;
+$string['collapsed_help'] = 'Si vous voulez que la première section du cours (la section 0 dans le terme de Moodle) soit fermé pour les étudiants, sélectionné oui. Défaut : Oui';
+$string['course_title'] = 'Titre du cours';
+$string['course_title_help'] = 'Imprimer le titre du cours sur la page principale du cours';
+$string['currentsection'] = 'Cette section';
+$string['cover_image'] = 'Image couverture';
+$string['cover_image_help'] = 'Ajouter une image qui sera utilisé sur la page couverture.';
+$string['editsection'] = 'Modifier la section';
+$string['deletesection'] = 'Effacer la section';
+$string['hidden'] = 'Non disponible pour les étudiants';
+$string['main_menu'] = 'Menu du cours';
+$string['numcolumns'] = 'Nombre de colonnes';
+$string['numcolumns_help'] = 'Sélectionnez le nombre de colonnes que vous souhaitez par ligne sur la page couverture du cours.';
+$string['return'] = 'Accueil';
+$string['sectionname'] = 'Section';
+$string['start_here'] = 'Commencez ici';
+$string['tab_label'] = 'Longueur de l\'étiquette';
+$string['tab_label_help'] = 'La longueur maximale d\'une étiquette doit avoir pour faire partie des onglets d\'une seule page. Si les étiquettes sont supérieurs à ce nombre, ils ne seront pas ajoutés sous forme d\'onglet. Cependant, ils seront affichés sous l\'onglet approprié.';
+$string['pluginname'] = 'Format Glendon';
+$string['section0name'] = 'Commencez ici';
+$string['page-course-view-glendon'] = 'Tous les pages pronicpale dans le format Glendon';
+$string['page-course-view-glendon-x'] = 'Tous les pages dasn le format Glendon';
+$string['hidefromothers'] = 'Cacher la section';
+$string['showfromothers'] = 'Montrer la section';
