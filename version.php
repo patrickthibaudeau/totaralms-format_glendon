51d8e2ebd1a593936c84dad5cfd8f8e8955e08aa
Patrick Thibaudeau <thibaud@yorku.ca>
2018-07-13 08:09:58 -0400

    Added privacy

--- version.php
+++ version.php
@@ -26,8 +26,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$plugin->version   = 2018071201;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2018071300;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2017051500;        // Requires this Moodle version.
-$plugin->release = '2.1 Build(2018071201)';
+$plugin->release = '2.2 Build(2018071300)';
 $plugin->component = 'format_glendon';    // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
